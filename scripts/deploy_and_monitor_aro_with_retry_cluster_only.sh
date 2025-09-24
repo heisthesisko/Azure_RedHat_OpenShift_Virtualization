bd3fe1fd39431cc584e1e12bb38a709050946931
heisthesisko <joel.sisko@outlook.com>
2025-09-23 19:14:56 -0700

    Update deployment script to reference validated manual upgrade for ARO cluster version 4.18

--- scripts/deploy_and_monitor_aro_with_retry_cluster_only.sh
+++ scripts/deploy_and_monitor_aro_with_retry_cluster_only.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPLOY_SCRIPT="./deploy_aro_cluster_validated.sh"
+DEPLOY_SCRIPT="./deploy_aro_cluster_validated_manual_upgrade_4-18.sh"
 MAX_RETRIES=3
 RETRY_DELAY=60  # in seconds
 LOG_FILE="aro_deployment_$(date +%Y%m%d_%H%M%S).log"
