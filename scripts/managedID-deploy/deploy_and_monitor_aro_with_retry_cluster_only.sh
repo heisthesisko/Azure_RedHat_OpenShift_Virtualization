fb2ed155357df9da97733eebbd14f2a57f49a1df
heisthesisko <joel.sisko@outlook.com>
2025-10-04 17:30:15 -0700

    script clean up

--- scripts/managedID-deploy/deploy_and_monitor_aro_with_retry_cluster_only.sh
+++ scripts/managedID-deploy/deploy_and_monitor_aro_with_retry_cluster_only.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPLOY_SCRIPT="./deploy_aro_cluster_managedid_preview_ver_001.sh"
+DEPLOY_SCRIPT="./build_aro_cluster_managedid_preview_ver_001.sh"
 MAX_RETRIES=3
 RETRY_DELAY=60  # in seconds
 LOG_FILE="aro_deployment_$(date +%Y%m%d_%H%M%S).log"
